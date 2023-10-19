1eb4cd3520bef0e409237d421cca98c6931a7f27
Michael McClimon <michael@mcclimon.org>
2023-10-19 18:45:34 -0400

    cleaner: import structure from perl version
    Mostly this is a to-do list so that I don't forget some part of the
    thing I actually want.

--- branchclean/cleaner.py
+++ branchclean/cleaner.py
@@ -9,27 +9,30 @@ class Cleaner:
         self.patch_ids = {}
 
     def run(self):
-        self.find_branches()
+        self.assert_local_ok()
+        self.do_initial_fetch()
+        self.read_refs()
         self.compute_main_patch_ids()
+        self.process_refs()
+        self.get_confirmation()
+        self.make_changes()
 
-        for branch in self.branches:
-            patch_id = branch.compute_patch_id()
-            if patch_id is None:
-                continue
+    def assert_local_ok(self):
+        pass
 
-            if commit := self.patch_ids.get(patch_id):
-                print(f"{branch.name.decode()} merged as {commit.decode()}")
+    def do_initial_fetch(self):
+        pass
 
-    def find_branches(self):
+    def read_refs(self):
         branches = run_git(
             [
                 "for-each-ref",
                 "--format=%(objectname) %(objecttype) %(refname) %(upstream)",
                 "refs/heads",
             ],
-        ).splitlines()
+        )
 
-        for line in branches:
+        for line in branches.splitlines():
             sha, kind, name, upstream = line.split(sep=b" ")
             if kind != b"commit":
                 continue
@@ -38,7 +41,6 @@ class Cleaner:
             self.branches.append(branch)
 
     def compute_main_patch_ids(self):
-        ids = {}
         print("computing patch ids...")
         # find the oldest sha for branches, compute patch ids for everything since
         oldest = min(map(lambda b: b.birth, self.branches))
@@ -50,6 +52,19 @@ class Cleaner:
                 continue
 
             patch_id, commit = line.split()
-            ids[patch_id] = commit
+            self.patch_ids[patch_id] = commit
 
-        self.patch_ids = ids
+    def process_refs(self):
+        for branch in self.branches:
+            patch_id = branch.compute_patch_id()
+            if patch_id is None:
+                continue
+
+            if commit := self.patch_ids.get(patch_id):
+                print(f"{branch.name.decode()} merged as {commit.decode()}")
+
+    def get_confirmation(self):
+        pass
+
+    def make_changes(self):
+        pass
